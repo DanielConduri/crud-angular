fb89aca9cfbe0454b6749246e5afde56975d9d95
DanielConduri <jonathand.tene@espoch.edu.ec>
2024-01-12 00:02:35 -0500

    configuración de la url y cors 7

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,28 @@
 import app from "./app.js";
 import {sequelize} from './database/database.js';
 
+app.get("/info", (req, res) => {
+    res.json({
+        status: true,
+        message: 'Welcome to the ExpressJS server',
+        //body: [{server:'recetas'}],
+        version: '1.0.0'
+    })
+  });
+
+  const port = process.env.PORT || 3002;
 async function main(){
     try {
         await sequelize.sync( { force: false }); //No recrea las tablas
         console.log('Connection has been established successfully.');
-        app.listen(3002)
-        console.log('Server is  listening on port', 3002)
+        app.listen(port, () => {
+            console.log("Server is listening on port", port);
+          });
+        //app.listen(3002)
+        //console.log('Server is  listening on port', 3002)
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
 }
 
-main();
+main(port);
