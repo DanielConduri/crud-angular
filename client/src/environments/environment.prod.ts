65a03c6dec1c67bcf424cac6236e0a4005a535ff
DanielConduri <jonathand.tene@espoch.edu.ec>
2024-01-11 23:28:18 -0500

    configuración de la url y cors 2

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: "https://crud-angular-8tnp.vercel.app/",
+  url: "https://crud-angular-2at2.vercel.app/",
   urlApi: "http://localhost:3002/",
   baseUrl: ""
 };
