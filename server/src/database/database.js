a8206ab0a84b63803c66498bc57642b94774123c
DanielConduri <jonathand.tene@espoch.edu.ec>
2024-01-11 23:03:10 -0500

    Cambios en el frontend2

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
 //Servidor PostgreSQL 15
-/*export const sequelize = new Sequelize(
+export const sequelize = new Sequelize(
     'db_ventas',
     'postgres',
     'backend',
@@ -10,16 +10,16 @@ import { Sequelize } from 'sequelize';
         logging: false,
         dialect: 'postgres'
     }
-);*/
-
-export const sequelize = new Sequelize(
-    "db_rest",
-    "postgres", 
-    "9iZy90V7vB0PLQfrGXnO", 
-    {
-     host: "containers-us-west-91.railway.app",
-     logging: false,
-     dialect: "postgres",
-     port: 7261
-   } 
 );
+
+// export const sequelize = new Sequelize(
+//     "db_rest",
+//     "postgres", 
+//     "9iZy90V7vB0PLQfrGXnO", 
+//     {
+//      host: "containers-us-west-91.railway.app",
+//      logging: false,
+//      dialect: "postgres",
+//      port: 7261
+//    } 
+// );
