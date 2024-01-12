877f993168af8789da60d619d78eb5ebe8d852e9
DanielConduri <jonathand.tene@espoch.edu.ec>
2024-01-12 00:31:26 -0500

    configuración de la url y cors 10

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: "https://crud-angular-2at2.vercel.app/",
-  urlApi: "https://crud-angular-production-4690.up.railway.app/",
+  url: 'https://crud-angular-2at2.vercel.app/',
+  urlApi: 'https://crud-angular-production-4690.up.railway.app/',
   baseUrl: ""
 };
