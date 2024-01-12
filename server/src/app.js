4a12a73ae9a0e630c26db28b6c7fb5df7838081c
DanielConduri <jonathand.tene@espoch.edu.ec>
2024-01-11 23:44:01 -0500

    configuración de la url y cors 3

--- server/src/app.js
+++ server/src/app.js
@@ -4,10 +4,20 @@ import cookieParser from "cookie-parser";
 import indexRoutes from "./routes/index.routes.js";
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://crud-angular-2at2.vercel.app/');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3002/');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(express.json());  //Para procesar datos en formato json
 const whiteList = [
     "http://localhost:4200",
     "http://localhost:51769",
+    "http://localhost:3002/",
     "https://crud-angular-2at2.vercel.app/",
     "https://localhost:4200",
     "https://inventario-espoch.rubenvn.com",
