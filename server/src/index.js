600a5cc0eaf709215bb6d9954b85f98483372203
DanielConduri <jonathand.tene@espoch.edu.ec>
2024-05-23 18:07:21 -0500

    servidor config 3

--- server/src/index.js
+++ server/src/index.js
@@ -10,32 +10,32 @@ app.get("/info", (req, res) => {
     })
   });
 
-//   const port = process.env.PORT || 3002;
-// async function main(){
-//     try {
-//         await sequelize.sync( { force: false }); //No recrea las tablas
-//         console.log('Connection has been established successfully.');
-//         app.listen(port, () => {
-//             console.log("Server is listening on port", port);
-//           });
-//         //app.listen(3002)
-//         //console.log('Server is  listening on port', 3002)
-//     } catch (error) {
-//         console.error('Unable to connect to the database:', error);
-//     }
-// }
+  const port = process.env.PORT || 3002;
+async function main(){
+    try {
+        await sequelize.sync( { force: false }); //No recrea las tablas
+        console.log('Connection has been established successfully.');
+        app.listen(port, () => {
+            console.log("Server is listening on port", port);
+          });
+        //app.listen(3002)
+        //console.log('Server is  listening on port', 3002)
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
 
-// main(port);
+main(port);
 
-import  http  from 'http';
-const PORT = 3002;
+// import  http  from 'http';
+// const PORT = 3002;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World!');
+// });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running at http://localhost:${PORT}/`);
+// });
