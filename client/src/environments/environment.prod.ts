59c2f5a55ac34af1e2a0b2738ab7e4b02352b368
DanielConduri <jonathand.tene@espoch.edu.ec>
2024-08-18 18:28:40 -0500

    Actualización API

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   url: 'https://crud-angular-2at2.vercel.app/',
-  urlApi: 'https://crud-angular-production-0d12.up.railway.app/',
+  urlApi: 'https://crud-angular-production-ed75.up.railway.app/',
   baseUrl: ""
 };
