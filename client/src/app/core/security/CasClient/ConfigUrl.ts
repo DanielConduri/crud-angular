0b765d8a9c9eb0e84b7f24aec1786ba0fbaf6950
DanielConduri <jonathand.tene@espoch.edu.ec>
2024-09-05 16:02:28 -0500

    Filtrado de datos v2

--- client/src/app/core/security/CasClient/ConfigUrl.ts
+++ client/src/app/core/security/CasClient/ConfigUrl.ts
@@ -6,7 +6,8 @@ export let CONFIG = {
     CASLOGOUT: 'https://seguridad.espoch.edu.ec/cas/logout?',
     CASVALIDATE: 'https://seguridad.espoch.edu.ec/cas/p3/serviceValidate?',
     REDIRECT_URI: environment.url,
-    LOGOUT_REDIRECT: environment.urlLogOut,
+    //LOGOUT_REDIRECT: environment.urlLogOut,
+    LOGOUT_REDIRECT: environment.url,
 
   // REDIRECT_URI: 'https://pruebacomprobante.espoch.edu.ec:8080/cash',
   //   LOGOUT_REDIRECT: 'https://pruebacomprobante.espoch.edu.ec:8080/logout',
