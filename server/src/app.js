38a4b2f0fa94f9485b00eae16940e298c40292a6
DanielConduri <jonathand.tene@espoch.edu.ec>
2024-11-08 10:42:48 -0500

    url update back 5

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,7 @@ import indexRoutes from "./routes/index.routes.js";
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://crud-angular-eta.vercel.app/');
+  res.header('Access-Control-Allow-Origin', 'https://crud-angular-eta.vercel.app');
   //res.header('Access-Control-Allow-Origin', 'http://localhost:3002/');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -22,7 +22,7 @@ const whiteList = [
     "https://crud-angular-2at2-kitl-danielconduris-projects.vercel.app/",
     "https://localhost:4200",
     "https://pruebasinventario.me",
-    "https://crud-angular-eta.vercel.app/"
+    "https://crud-angular-eta.vercel.app"
     //"https://inventario-espoch.rubenvn.com",
   ];
   app.use(
