98e326e75c06ebf8dfd03c0eab010a6e2ed216e3
DanielConduri <jonathand.tene@espoch.edu.ec>
2024-11-08 10:40:22 -0500

    url update back 4

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://crud-angular-eta.vercel.app/');
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3002/');
+  //res.header('Access-Control-Allow-Origin', 'http://localhost:3002/');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
