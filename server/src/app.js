098b019b88dcc25bc912067d57874f2b71ba4c5d
DanielConduri <jonathand.tene@espoch.edu.ec>
2024-11-08 10:35:31 -0500

    url update back 2

--- server/src/app.js
+++ server/src/app.js
@@ -5,13 +5,13 @@ import indexRoutes from "./routes/index.routes.js";
 
 const app = express();
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://crud-angular-2at2.vercel.app/');
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:3002/');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://crud-angular-eta.vercel.app/');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3002/');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.use(express.json());  //Para procesar datos en formato json
 const whiteList = [
@@ -20,7 +20,6 @@ const whiteList = [
     "http://localhost:3002/",
     "https://crud-angular-production-315c.up.railway.app",
     "https://crud-angular-2at2-kitl-danielconduris-projects.vercel.app/",
-    "https://crud-angular-eta.vercel.app/",
     "https://localhost:4200",
     "https://pruebasinventario.me"
     //"https://inventario-espoch.rubenvn.com",
