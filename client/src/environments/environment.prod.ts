926bac7650f0724d0b18e8648902f36854d2a6f0
DanielConduri <jonathand.tene@espoch.edu.ec>
2024-12-16 17:17:37 -0500

    update client 2

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   url: 'https://crud-angular-eta.vercel.app/',
-  urlApi: 'https://crud-angular-production-ed75.up.railway.app/',
+  urlApi: 'https://pruebasinventario.me/apiv4/',
   baseUrl: ""
 };
