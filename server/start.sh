d576f8eedc3c4cb235b9d36fdd70334e5c0e4d57
DanielConduri <jonathand.tene@espoch.edu.ec>
2025-01-02 22:00:58 -0500

    test logs configure failed 7.1

--- server/start.sh
+++ server/start.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-npm test && npm run coverage && npm run dev
+#!/bin/bash
+# Ejecutar los tests
+npm test
+if [ $? -eq 0 ]; then
+    echo "Tests pasaron, iniciando el servidor..."
+    npm start
+else
+    echo "Los tests fallaron, deteniendo el contenedor."
+    exit 1
+fi
