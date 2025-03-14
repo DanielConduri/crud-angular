ac260190312215bb00d179444c6a47ca3ca73594
DanielConduri <jonathand.tene@espoch.edu.ec>
2025-03-14 16:07:29 -0500

    url update

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: 'https://app.pruebasinventario.me/',
-  urlApi: 'https://app.pruebasinventario.me/apiv4/',
+  url: 'https://pruebasinventario.me/',
+  urlApi: 'https://pruebasinventario.me/apiv4/',
   baseUrl: ""
 };
